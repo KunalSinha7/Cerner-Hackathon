bc3097a320ab64aabf8b480d2c841a2b0e81f122
JP061142 <Jimmy.Patterson@cerner.com>
2019-07-16 00:54:36 -0500

    spotifyroutes ++

--- server/api/spotifyRoutes/spotifyRoutes.js
+++ server/api/spotifyRoutes/spotifyRoutes.js
@@ -6,4 +6,14 @@ router.get("/", (req, res) => {
   res.send("Routes that use Spotify");
 });
 
+router.post("/user", async (req, res) => {
+    try {
+        let token = req.body.SpotifyToken;
+        let data = await axios.get("https://api.spotify.com/v1/me?access_token=" + token);
+        console.log(data);
+    } catch ((err) => {
+        console.log("Error: ", err);
+    })
+});
+
 module.exports = router;
