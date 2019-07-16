d010711e73f3e932e9967cea9fb5108dd765423a
Sarah Warren <sarah.warren@cerner.com>
2019-07-16 07:37:11 -0500

    merge into self

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -5,9 +5,13 @@ const User = require('../models/user');
 exports.user_create = (req, res, next) => {
   const user = new User({
     _id : new mongoose.Types.ObjectId,
+<<<<<<< Updated upstream
     user_id : 345772,
     song_id : 687537,
     ranking : 'A'
+=======
+
+>>>>>>> Stashed changes
   });
   console.log(user);
   res.send(user);
--- server/api/models/user.js
+++ server/api/models/user.js
@@ -3,10 +3,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+<<<<<<< Updated upstream
     _id : { type: Schema.Types.ObjectId },
     user_id : { type: Number, required: true },
     song_id : { type: Number, required: true },
     ranking : { type: String }
+=======
+    _id : Schema.Types.ObjectId,
+    user_id : Number,
+    song_id : Number,
+    ranking : String
+>>>>>>> Stashed changes
 });
 
 // Export model.
